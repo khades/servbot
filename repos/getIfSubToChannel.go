99d739261517ca2c0fcbeaa3b1e55ed1befe5e12
khades <khades@khades.org>
2016-11-16 20:34:26 +0700

    cache fix

--- repos/getIfSubToChannel.go
+++ repos/getIfSubToChannel.go
@@ -3,5 +3,9 @@ package repos
 // GetIfSubToChannel is cache for checking if user subbed to channel
 func GetIfSubToChannel(user *string, channel *string) (bool, bool) {
 	value, found := cacheObject.Get(*user + *channel + "isSub")
-	return value.(bool), found
+	if found {
+		return value.(bool), true
+
+	}
+	return false, false
 }
