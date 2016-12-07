ffbb67b08dfb0ccb52199bb981a07467ba387f34
khades <khades@khades.org>
2016-12-08 02:02:01 +0700

    debugging sessions

--- repos/sessionStore.go
+++ repos/sessionStore.go
@@ -4,11 +4,13 @@ import (
 	"net/http"
 
 	"github.com/gorilla/sessions"
-	mongostore "gopkg.in/go-playground/mongostore.v4"
 )
 
-var sessionStore = mongostore.New(dbSession, "sessions", &sessions.Options{MaxAge: 3600, Secure: true}, true,
-	[]byte("secret-key"))
+//mongostore "gopkg.in/go-playground/mongostore.v4"
+
+//var sessionStore = mongostore.New(dbSession, "sessions", &sessions.Options{MaxAge: 3600, Secure: true}, true,
+//[]byte("secret-key"))
+var sessionStore = sessions.NewCookieStore([]byte("ASFasfafasfasfSfAS"))
 
 // GetSession is configured object for reading http sessions
 func GetSession(r *http.Request) (*sessions.Session, error) {
