6bd21144d55022416310528572114a673653eba0
khades <khades@khades.org>
2017-03-08 17:26:04 +0700

    Fixing oauth v5

--- httpbackend/oauth.go
+++ httpbackend/oauth.go
@@ -78,7 +78,7 @@ func oauth(w http.ResponseWriter, r *http.Request) {
 
 	if nameResp.StatusCode == 400 {
 		body, err := ioutil.ReadAll(resp.Body)
-
+		log.Println(string(body))
 		if err == nil {
 			log.Println(string(body))
 		} else {
