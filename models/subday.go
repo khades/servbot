f723bb2b2ade2ba04b45a41fcd157a3a59c5a4fd
khades <khades@khades.org>
2017-12-22 01:40:49 +0700

    Fix: SubtrainCounter

---
+++ models/subday.go
@@ -0,0 +1,25 @@
+package models
+
+import (
+	"time"
+)
+
+type SubdayRecord struct {
+	User   string `json:"user"`
+	UserID string `json:"userID"`
+	Game   string `json:"game"`
+}
+type SubdayWinnersHistory struct {
+	Date    time.Time      `json:"date"`
+	Winners []SubdayRecord `json:"winners"`
+}
+type Subday struct {
+	ChannelID      string                 `json:"channelID"`
+	IsActive       bool                   `json:"isActive"`
+	SubsOnly       bool                   `json:"subsOnly"`
+	Name           string                 `json:"name"`
+	Date           time.Time              `json:"date"`
+	Votes          []SubdayRecord         `json:"votes"`
+	Winners        []SubdayRecord         `json:"winners"`
+	WinnersHistory []SubdayWinnersHistory `json:"winnersHistory"`
+}
