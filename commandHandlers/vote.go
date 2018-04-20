73263c2fb75f4803b6e870a5b7df126b671006e7
khades <khades@khades.org>
2018-04-20 07:43:33 +0700

    Fix: vote chat handler is now debounced

--- commandHandlers/vote.go
+++ commandHandlers/vote.go
@@ -9,23 +9,30 @@ import (
 )
 
 func Vote(online bool, chatMessage *models.ChatMessage, chatCommand models.ChatCommand, ircClient *ircClient.IrcClient) {
-    if utf8.RuneCountInString(chatMessage.MessageBody) < 2 {
+	if utf8.RuneCountInString(chatMessage.MessageBody) < 2 {
 		return
 	}
 	game := chatMessage.MessageBody[1:]
 	subday, subdayError := repos.GetLastActiveSubday(&chatMessage.ChannelID)
+
 	if subdayError != nil {
-		ircClient.SendPublic(&models.OutgoingMessage{
-			Channel: chatMessage.Channel,
-			Body:    "Сабдей ещё не запущен SMOrc",
-			User:    chatMessage.User})
+		ircClient.SendDebounced(models.OutgoingDebouncedMessage{
+			Message: models.OutgoingMessage{
+				Channel: chatMessage.Channel,
+				User:    chatMessage.User,
+				Body:    "Сабдей ещё не запущен SMOrc"},
+			Command:    "voteCommand",
+			RedirectTo: chatMessage.User})
 		return
 	}
 	if subday.SubsOnly == true && chatMessage.IsSub == false {
-		ircClient.SendPublic(&models.OutgoingMessage{
-			Channel: chatMessage.Channel,
-			Body:    "Ты не саб SMOrc",
-			User:    chatMessage.User})
+		ircClient.SendDebounced(models.OutgoingDebouncedMessage{
+			Message: models.OutgoingMessage{
+				Channel: chatMessage.Channel,
+				User:    chatMessage.User,
+				Body:    "Ты не саб SMOrc"},
+			Command:    "voteCommand",
+			RedirectTo: chatMessage.User})
 		return
 	}
 	repos.VoteForSubday(&chatMessage.User, &chatMessage.UserID, &subday.ID, &game)
