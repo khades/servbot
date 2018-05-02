ca4f4912e085207dd4fcc32b75fa541cf9fc0636
khades <khades@khades.org>
2018-05-02 14:28:19 +0700

    Maintenance: DO NOT USE, prerelease

--- ircClient/ircClient.go
+++ ircClient/ircClient.go
@@ -118,8 +118,10 @@ func (ircClient *IrcClient) SendPrivate(message *models.OutgoingMessage) {
 
 // SendModsCommand runs mod command
 func (ircClient *IrcClient) SendModsCommand() {
-	if len(repos.Config.Channels) == 0
-	return
+	if len(repos.Config.Channels) == 0 {
+		return
+
+	}
 	channelName := repos.Config.Channels[ircClient.ModChannelIndex]
 	if channelName != "" {
 		ircClient.SendPublic(&models.OutgoingMessage{Channel: channelName, Body: "/mods"})
