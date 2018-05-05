401d5299d85ec01af63bc46d0ee86b3f957a6f6b
khades <khades@khades.org>
2018-05-06 01:41:11 +0700

    Maintenance: DO NOT USE, prerelease

--- services/checkFollowers.go
+++ services/checkFollowers.go
@@ -53,3 +53,39 @@ func AnnounceFollowers() {
 	}
 
 }
+
+func processOneChannel(channel models.FollowersToGreet) {
+	logger := logrus.WithFields(logrus.Fields{
+		"package": "services",
+		"feature": "followers",
+		"action":  "processOneChannel"})
+	defer repos.ResetFollowersToGreetOnChannel(&channel.ChannelID)
+	logger.Debugf("Processing channel %s", channel.ChannelID)
+	alertInfo, alertError := repos.GetSubAlert(&channel.ChannelID)
+	if alertError != nil {
+		logger.Debugf("No alert for channel %s", channel.ChannelID)
+
+		return
+	}
+	channelInfo, channelInfoError := repos.GetChannelInfo(&channel.ChannelID)
+	if channelInfoError != nil {
+		logger.Debugf("No channelInfo for channel %s", channel.ChannelID)
+
+		return
+	}
+	followers := []string{}
+	followersMap, followersError := repos.GetUsernames(channel.Followers)
+	if followersError != nil {
+		logger.Debugf("Followers resolve failed for channel %s", channel.ChannelID)
+
+		return
+	}
+	for _, follower := range *followersMap {
+		followers = append(followers, follower)
+	}
+	if channelInfoError == nil && alertInfo.Enabled == true && alertInfo.FollowerMessage != "" {
+		bot.IrcClientInstance.SendPublic(&models.OutgoingMessage{
+			Channel: channelInfo.Channel,
+			Body:    "@" + strings.Join(followers, " @") + " " + alertInfo.FollowerMessage})
+	}
+}
